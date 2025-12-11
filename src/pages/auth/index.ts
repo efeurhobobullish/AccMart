4a32fc90377941f2ecf009a32332381324b50532
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-12-12 00:49:51 +0100

    Update index.ts

--- src/pages/auth/index.ts
+++ src/pages/auth/index.ts
@@ -1,3 +1,4 @@
+export { default as Signup } from "./signup";
 export { default as Pin } from "./pin";
 export { default as Setup } from "./setup";
 export { default as ForgotPasscode } from "./forgot-passcode";
